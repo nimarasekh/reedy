a4508e3273b03b1cd8154da2b4dc212a85769d81
Aras Ergus <arasergus@posteo.net>
2026-06-09 05:23:41 +0200

    Prove retract stability of intersections

--- Reedy/MorphismProperty/Retracts.lean
+++ Reedy/MorphismProperty/Retracts.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2026 Joël Riou. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Joël Riou
+Authors: Joël Riou, Aras Ergus
 -/
 module
 
@@ -14,12 +14,13 @@ public import Mathlib.CategoryTheory.MorphismProperty.Retract
 
 @[expose] public section
 
-namespace CategoryTheory.MorphismProprerty
+namespace CategoryTheory.MorphismProperty
 
 instance {C : Type*} [Category* C] {ι : Type*} (W : ι → MorphismProperty C)
     [∀ i, (W i).IsStableUnderRetracts] :
     (⨅ i, W i).IsStableUnderRetracts where
   of_retract h hf := by
-    sorry
+    rw [MorphismProperty.iInf_iff] at hf ⊢
+    exact fun i => MorphismProperty.of_retract h (hf i)
 
-end CategoryTheory.MorphismProprerty
+end CategoryTheory.MorphismProperty
