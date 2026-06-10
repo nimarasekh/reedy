0b054ad1b66ae058d37cd30485a455d54abdbcce
Julian Kuelshammer <julian.kuelshammer@math.uu.se>
2026-06-10 10:41:38 +0200

    proofs of lt1 and lt2 for SimplexCategory

--- Reedy/Reedy/SimplexCategory.lean
+++ Reedy/Reedy/SimplexCategory.lean
@@ -20,6 +20,7 @@ open CategoryTheory MorphismProperty
 
 namespace SimplexCategory
 
+
 -- C.4.4
 -- claim https://github.com/joelriou/reedy/issues/22 if working on this
 def reedyStructure :
@@ -27,8 +28,26 @@ def reedyStructure :
 -- the proof should follow from ingredients in the file
 -- `Mathlib.AlgebraicTopology.SimplexCategory.Basic`
   deg := len
-  lt₁ := sorry
-  lt₂ := sorry
+  lt₁ f hepi hnonid := by
+    haveI := (epimorphisms.iff f).mp hepi
+    apply lt_of_le_of_ne (len_le_of_epi f)
+    intro h
+    have heq := SimplexCategory.ext_iff.mpr h
+    subst heq
+    have := eq_id_of_epi f
+    apply hnonid
+    rw [this]
+    exact ofHoms.mk _
+  lt₂ f hmono hnonid := by
+    haveI := (monomorphisms.iff f).mp hmono
+    apply lt_of_le_of_ne (len_le_of_mono f)
+    intro h
+    have heq := SimplexCategory.ext_iff.mpr h
+    subst heq
+    have := eq_id_of_mono f
+    apply hnonid
+    rw [this]
+    exact ofHoms.mk _
   nonempty_unique := sorry
 
 end SimplexCategory
